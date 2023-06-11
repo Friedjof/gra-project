8cb477e9b573a93310998ffc54973592e14a5e8e
Friedjof <friedjof@noweck.info>
2023-06-12 00:05:09 +0200

    add: fib tests

---
+++ tests/utils/fib.c
@@ -0,0 +1,10 @@
+#include <criterion/criterion.h>
+
+#include "utils.h"
+
+Test(fibtests, some_values) {
+    cr_assert_eq(fib(10), 55);
+    cr_assert_eq(fib(11), 89);
+    cr_assert_eq(fib(12), 144);
+    cr_assert_eq(fib(13), 233);
+}
